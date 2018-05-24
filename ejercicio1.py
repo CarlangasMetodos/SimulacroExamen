70e460ab0c244707cf9f6f319873da7d731debb3
carlangasmetodos-simulacroexamen-gola5p90 <carlangasmetodos-simulacroexamen-gola5p90@local>
2018-05-24 03:56:01 +0000

    Simulacro examen

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,2 +1,17 @@
 #Escriba el codigo necesario para que al ejecutar
 #python ejercicio1.py se impriman los enteros del 10 al 1 en cuenta regresiva.
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+lista=[]
+for i in range (11):
+    lista.append(i)
+    
+num=0    
+for i in lista:
+    num+=1
+    print (lista[-num])
+    
+
+    
--- ejercicio2.py
+++ ejercicio2.py
@@ -1,3 +1,18 @@
 # Escriba el codigo necesario para que al ejecutar python ejercicio2.py
 # se cree una grafica con una funcion sinusoidal entre 0 y 2pi.
 
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+def seno(x):
+    return np.sin(x)
+
+x=np.linspace(0.0,2.0*np.pi,100)
+y=seno(x)
+
+plt.figure()
+plt.plot(x,y)
+plt.savefig('Graph.png')
+plt.show()
